b7c6315adac7b58b2f72e85e3f38bf3c8b3fed49
YiJing <yijing2055@gmail.com>
2025-02-28 15:57:18 +0800

    Fix：調整404頁面樣式，並新增link跳轉至首頁的功能設定

--- src/pages/Front-end/NotFoundPage.jsx
+++ src/pages/Front-end/NotFoundPage.jsx
@@ -1,4 +1,12 @@
-export default function NotFoundPage(){
-  return <h1 className="mt-5 text-center text-secondary"> 404 此頁面不存在 </h1>
+import { Link } from "react-router-dom"
 
+export default function NotFoundPage(){
+  return(
+   <>
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <h1 className="mt-5 text-center text-secondary"> 404 此頁面不存在 </h1>
+      <Link to='/' className="btn btn-outline-primary mt-3" > 返回首頁 </Link>
+    </div>
+   </>
+  )
 }
